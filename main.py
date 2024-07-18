9c1569a60dd1d470dcbad0d7afff02e58ca97527
lesshishkin <lesshishkin@gmail.com>
2024-07-18 21:55:56 +0300

    yttm

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import youtokentome as yttm
 
+
 def create_tokenizer():
-    train_data_path = "train_data.txt"
-    model_path = "example.model"
+    train_data_path = "data/tinystories/train_v2_ru_prepared.txt"
+    model_path = "data/tinystories/ru_tinystories_tokenizer.model"
 
     yttm.BPE.train(data=train_data_path, vocab_size=25000, model=model_path)
 
+def load_tokenizer(model_path):
+    # Loading model
+    bpe = yttm.BPE(model=model_path)
+    #
+    # test_text = 'Пришла жаба в гости к хрбше, а Том сидел в углу и думал'
+    # # Two types of tokenization
+    # print(bpe.encode([test_text], output_type=yttm.OutputType.ID))
+    # print(bpe.encode([test_text], output_type=yttm.OutputType.SUBWORD))
+
+    # Получение словаря
+    vocab = bpe.vocab()
+
+    # Сохранение словаря в файл
+    with open('vocab.txt', 'w') as f:
+        for token in vocab:
+            f.write(f"{token}\n")
+
 
 if __name__ == '__main__':
-    create_tokenizer()
+    load_tokenizer('data/tinystories/ru_tinystories_tokenizer.model')
 
