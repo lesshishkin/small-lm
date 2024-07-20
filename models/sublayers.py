3b2c90df67e2ce8f8547cf5d3564dcb3d7db726e
lesshishkin <lesshishkin@gmail.com>
2024-07-20 12:40:25 +0300

    ff llama3, deleted unused calsses

--- models/sublayers.py
+++ models/sublayers.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 from typing import Optional, Tuple
 from models.modules import ScaledDotProductAttention, ScaledDotProductAttentionSimple
+import torch.nn.functional as F
 
 
 class MultiHeadAttention(nn.Module):
@@ -175,16 +176,18 @@ class FeedForwardLLaMA3(nn.Module):
         super().__init__()
         hidden_dim = int(2 * hidden_dim / 3)
         # custom dim factor multiplier
+        # todo разобраться почему три слоя, какие размеры
         if ffn_dim_multiplier is not None:
             hidden_dim = int(ffn_dim_multiplier * hidden_dim)
         hidden_dim = multiple_of * ((hidden_dim + multiple_of - 1) // multiple_of)
 
-        self.w1 = ColumnParallelLinear(
-            dim, hidden_dim, bias=False, gather_output=False, init_method=lambda x: x
-        )
-        self.w2 = RowParallelLinear(
-            hidden_dim, dim, bias=False, input_is_parallel=True, init_method=lambda x: x
-        )
-        self.w3 = ColumnParallelLinear(
-            dim, hidden_dim, bias=False, gather_output=False, init_method=lambda x: x
-        )
+        self.w1 = nn.Linear(dim, hidden_dim, bias=False)
+        self.w2 = nn.Linear(hidden_dim, dim, bias=False)
+        self.w3 = nn.Linear(dim, hidden_dim, bias=False)
+
+        nn.init.xavier_uniform_(self.w1.weight)
+        nn.init.xavier_uniform_(self.w2.weight)
+        nn.init.xavier_uniform_(self.w2.weight)
+
+    def forward(self, x):
+        return self.w2(F.silu(self.w1(x)) * self.w3(x))
