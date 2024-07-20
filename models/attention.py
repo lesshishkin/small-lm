67bf47a254ba03ea636adddcc1ca2d64675127d4
lesshishkin <lesshishkin@gmail.com>
2024-07-20 13:54:09 +0300

    attention

--- models/attention.py
+++ models/attention.py
@@ -35,6 +35,19 @@ def apply_rotary_emb(
     return xq_out.type_as(xq), xk_out.type_as(xk)
 
 
+def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor:
+    """torch.repeat_interleave(x, dim=2, repeats=n_rep)"""
+    # todo check
+    bs, slen, n_kv_heads, head_dim = x.shape
+    if n_rep == 1:
+        return x
+    return (
+        x[:, :, :, None, :]
+        .expand(bs, slen, n_kv_heads, n_rep, head_dim)
+        .reshape(bs, slen, n_kv_heads * n_rep, head_dim)
+    )
+
+
 class Attention(nn.Module):
     """Attention like LLaMA3, but without KV cache and parallelism"""
     # todo remove KV cache, remove parallel, fi devices
@@ -138,4 +151,56 @@ class Attention(nn.Module):
         scores = F.softmax(scores.float(), dim=-1).type_as(xq)
         output = torch.matmul(scores, values)  # (bs, n_local_heads, seqlen, head_dim)
         output = output.transpose(1, 2).contiguous().view(bsz, seqlen, -1)
+        return self.wo(output)
+
+
+class Attention(nn.Module):
+    """Attention like LLaMA3 without KV cache and parallelism"""
+    def __init__(self, config):
+        super().__init__()
+        # GQMA
+        self.n_kv_heads = config.model.n_heads if config.model.n_kv_heads is None else config.model.n_kv_heads
+        self.n_heads = config.model.n_heads
+        self.n_rep = self.n_heads // self.n_kv_heads
+        self.head_dim = config.model.dim // config.model.n_heads
+
+        self.wq = nn.Linear(config.model.dim, config.model.n_heads * self.head_dim, bias=False)
+        self.wk = nn.Linear(config.model.dim, self.n_kv_heads * self.head_dim, bias=False)
+        self.wv = nn.Linear(config.model.dim, self.n_kv_heads * self.head_dim, bias=False)
+        self.wo = nn.Linear(config.model.n_heads * self.head_dim, config.model.dim, bias=False)
+
+        nn.init.xavier_uniform_(self.wq.weight)
+        nn.init.xavier_uniform_(self.wk.weight)
+        nn.init.xavier_uniform_(self.wv.weight)
+        nn.init.xavier_uniform_(self.wo.weight)
+
+    def forward(self, x: torch.Tensor, start_pos: int, freqs_cis: torch.Tensor, mask: Optional[torch.Tensor]):
+        bsz, seqlen, _ = x.shape
+        xq, xk, xv = self.wq(x), self.wk(x), self.wv(x)
+
+        xq = xq.view(bsz, seqlen, self.n_heads, self.head_dim)
+        xk = xk.view(bsz, seqlen, self.n_kv_heads, self.head_dim)
+        xv = xv.view(bsz, seqlen, self.n_kv_heads, self.head_dim)
+
+        xq, xk = apply_rotary_emb(xq, xk, freqs_cis=freqs_cis)
+
+        keys = xk
+        values = xv
+
+        # GQMA
+        # todo  check dims
+        # repeat k/v heads if n_kv_heads < n_heads
+        keys = repeat_kv(keys, self.n_rep)  # (bs, seqlen, n_local_heads, head_dim)
+        values = repeat_kv(values, self.n_rep)  # (bs, seqlen, n_local_heads, head_dim)
+
+        xq = xq.transpose(1, 2)  # (bs, n_local_heads, seqlen, head_dim)
+        keys = keys.transpose(1, 2)  # (bs, n_local_heads, seqlen, head_dim)
+        values = values.transpose(1, 2)  # (bs, n_local_heads, seqlen, head_dim)
+        scores = torch.matmul(xq, keys.transpose(2, 3)) / sqrt(self.head_dim)
+        if mask is not None:
+            scores = scores + mask * torch.tensor(-1e9)  # todo check
+        scores = F.softmax(scores.float(), dim=-1).type_as(xq)
+        output = torch.matmul(scores, values)  # (bs, n_local_heads, seqlen, head_dim)
+        output = output.transpose(1, 2).contiguous().view(bsz, seqlen, -1)
+
         return self.wo(output)
