9ac5b44e2fd29b416df773fc6f15c234430ed7d5
lesshishkin <lesshishkin@gmail.com>
2024-07-20 13:59:12 +0300

    attention config

--- configs/model_config.py
+++ configs/model_config.py
@@ -3,12 +3,12 @@ from easydict import EasyDict
 model_cfg = EasyDict()
 model_cfg.name = 'TinyLLM'
 
-model_cfg.d_model = 512
+model_cfg.dim = 512
 model_cfg.d_ff = 2048
 model_cfg.max_sequence_length = 1024
-model_cfg.heads_num = 8
+model_cfg.n_heads = 8
+model_cfg.n_kv_heads = 4    # GQMA
 model_cfg.layers_num = 6
-model_cfg.eps = 1e-5
+model_cfg.eps = 1e-6
 model_cfg.dropout_rate = 0.1
-model_cfg.activation = 'GELU'  # from (ReLU, GELU)
-model_cfg.attention_bias = False
+
