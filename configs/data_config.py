6ba54a10f9ab3f9737794bd8e783369fe928a8d7
lesshishkin <lesshishkin@gmail.com>
2024-07-21 19:45:00 +0300

    for kaggle paths

--- configs/data_config.py
+++ configs/data_config.py
@@ -5,7 +5,7 @@ import os
 # TinyStories Dataset
 data_cfg = EasyDict()
 data_cfg.name = 'TinyStoriesDataset'
-data_cfg.path_to_data = 'data/tinystories'
+data_cfg.path_to_data = '/kaggle/input/tinystories'
 data_cfg.tokenized_train_data_path = os.path.join(data_cfg.path_to_data, 'train_v2_en_tokenized.pickle')
 data_cfg.tokenized_valid_data_path = os.path.join(data_cfg.path_to_data, 'valid_v2_en_tokenized.pickle')
 data_cfg.vocabulary_size = 25_000
--- dataset/russian_stories_dataset.py
+++ dataset/russian_stories_dataset.py
@@ -12,12 +12,10 @@ class TinyStoriesDataset(Dataset):
         self.set_type = set_type
 
         if self.set_type == SetType.train:
-            self.dataset = read_file(os.path.join(self.config.path_to_data,
-                                                  self.config.tokenized_train_data_path))
+            self.dataset = read_file(config.tokenized_train_data_path)
 
         elif self.set_type == SetType.validation:
-            self.dataset = read_file(os.path.join(self.config.path_to_data,
-                                                  self.config.tokenized_valid_data_path))
+            self.dataset = read_file(config.tokenized_valid_data_path)
 
         else:
             raise Exception('Unknown set type')
