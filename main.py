ac3ce95060494a880eef209ab1bac6264adcd7a0
Emil Shagiev <lesshishkin@gmail.com>
2024-07-21 18:51:55 +0300

    main, gitignore

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ import youtokentome as yttm
 
 
 if __name__ == '__main__':
-    # trainer = Trainer(experiment_cfg)
-    # trainer.batch_overfit()
-    dataset = "data/tinystories/valid_v2_en.txt"
-    bpe_model = "data/tinystories/en_tokenizer.model"
-    tokenized_dataset = "data/tinystories/valid_v2_en_tokenized.pickle"
-    vocab_file = "data/tinystories/en_vocab.txt"
-    vocab_size = 25_000
-
-    create_vocab_file(bpe_model, vocab_file)
+    # # trainer = Trainer(experiment_cfg)
+    # # trainer.batch_overfit()
+    # dataset = "data/tinystories/valid_v2_en.txt"
+    # bpe_model = "data/tinystories/en_tokenizer.model"
+    # tokenized_dataset = "data/tinystories/valid_v2_en_tokenized.pickle"
+    # vocab_file = "data/tinystories/en_vocab.txt"
+    # vocab_size = 25_000
+    #
+    # create_vocab_file(bpe_model, vocab_file)
+    trainer = Trainer(experiment_cfg)
+    trainer.batch_overfit()
