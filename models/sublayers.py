ac49978c061a2cc3a44b1aa1efb72ad15134ac81
lesshishkin <lesshishkin@gmail.com>
2024-07-25 17:34:09 +0300

    tinyllm2

--- models/sublayers.py
+++ models/sublayers.py
@@ -48,3 +48,72 @@ class FeedForward(nn.Module):
 
     def forward(self, x):
         return self.w2(F.silu(self.w1(x)) * self.w3(x))
+
+
+class FeedForward2(nn.Module):
+    """A class for implementing Feed Forward layer.
+
+    For each sample this block makes two linear transformations defined as follows:
+            FF(x) = max(0, x * W_1 + b_1) * W_2 + b_2,
+
+            where:
+                - x is an input tensor of shape (sequence length, d_model)
+                - W_1 and b_1 are trainable parameters of first Linear layer
+                        with output features num = d_ff and input features num = d_model
+                - W_2 and b_2 are trainable parameters of first Linear layer
+                        with output features num = d_model and input features num = d_ff
+    """
+
+    def __init__(self, config):
+        """Layers initialization."""
+        super(FeedForward2, self).__init__()
+        self.weights_1 = nn.Linear(config.dim, config.d_ff, bias=False)
+        self.weights_2 = nn.Linear(config.d_ff, config.dim, bias=False)
+        self.gelu = nn.GELU()
+
+        self._init_weights()
+
+    def _init_weights(self):
+        """Weights initialization."""
+        nn.init.xavier_uniform_(self.weights_1.weight)
+        # nn.init.zeros_(self.weights_1.bias)
+        nn.init.xavier_uniform_(self.weights_2.weight)
+        # nn.init.zeros_(self.weights_2.bias)
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        """Forward pass for the Feed-Forward layer.
+
+        Args:
+            inputs: tensor of shape (batch size, sequence length, d_model).
+
+        Returns:
+            Tensor of shape (batch size, sequence length, d_model)
+        """
+        return self.weights_2(self.gelu(self.weights_1(inputs)))
+
+
+class LayerNorm(nn.Module):
+    """A class for implementing Layer normalization."""
+
+    def __init__(self, config):
+        """Parameters initialization."""
+        super(LayerNorm, self).__init__()
+
+        self.gamma = nn.Parameter(torch.ones(config.dim))
+        self.beta = nn.Parameter(torch.zeros(config.dim))
+        self.eps = config.eps
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        """Forward pass for the Layer normalization block.
+
+        Args:
+            inputs: tensor of shape (batch_size, sequence length, d_model)
+
+        Returns:
+            Tensor of shape (batch size, sequence length, d_model)
+        """
+        mean = inputs.mean(-1, keepdim=True)
+        var = inputs.var(-1, keepdim=True, unbiased=False)
+        normalized_inputs = (inputs - mean) / torch.sqrt(var + self.eps)
+
+        return self.gamma * normalized_inputs + self.beta
---
+++ models/tinyllm2.py
@@ -0,0 +1,81 @@
+import torch
+from torch import nn
+from models.attention import Attention2
+from models.sublayers import LayerNorm, FeedForward2
+import torch.nn.functional as F
+from typing import Optional, Tuple
+
+
+class TransformerBlock2(nn.Module):
+    def __init__(self, layer_id: int, config):
+        super(TransformerBlock2, self).__init__()
+        self.n_heads = config.n_heads
+        self.dim = config.dim
+        self.attention = Attention2(config)
+        self.feed_forward = FeedForward2(config)
+        self.layer_id = layer_id
+        self.attention_norm = LayerNorm(config)
+        self.ffn_norm = LayerNorm(config)
+        self.ff_dropout = nn.Dropout(config.dropout_rate)
+        self.attention_dropout = nn.Dropout(config.dropout_rate)
+
+    def forward(
+        self,
+        x: torch.Tensor,
+        start_pos: int,
+        freqs_cis: torch.Tensor,
+        mask,
+    ):
+        h = x + self.attention_dropout(self.attention(self.attention_norm(x), start_pos, freqs_cis, mask))
+        out = h + self.ff_dropout(self.feed_forward(self.ffn_norm(h)))
+
+        return out
+
+
+class TinyLLM2(nn.Module):
+    """TinyLLM -- tiny but large. Like LLaMA3"""
+    def __init__(self, config, vocab_size, device):
+        super(TinyLLM2, self).__init__()
+
+        self.config = config
+        self.vocab_size = vocab_size
+        self.n_layers = config.n_layers
+
+        self.tok_embeddings = nn.Embedding(self.vocab_size, config.dim, padding_idx=0, device=device)
+
+        self.layers = torch.nn.ModuleList()
+        for layer_id in range(config.n_layers):
+            self.layers.append(TransformerBlock2(layer_id, config))
+
+        self.norm = LayerNorm(config)
+        self.output = nn.Linear(config.dim, self.vocab_size, bias=False)
+
+        self.freqs_cis = self.precompute_freqs_cis(
+            config.dim // config.n_heads,
+            config.max_seq_len * 2,
+            device,
+            config.rope_theta,
+        )
+
+    @staticmethod
+    def precompute_freqs_cis(dim: int, end: int, device, theta: float = 10000.0):
+        """For RoPE. Заранее считаем комплексные множители"""
+        freqs = 1.0 / (theta ** (torch.arange(0, dim, 2, device=device)[: (dim // 2)].float() / dim))
+        t = torch.arange(end, device=device, dtype=torch.float32)
+        freqs = torch.outer(t, freqs)
+        freqs_cis = torch.polar(torch.ones_like(freqs), freqs)  # complex64
+        return freqs_cis
+
+    def forward(self, tokens: torch.Tensor, start_pos: int, mask):
+        _bsz, seqlen = tokens.shape
+        h = self.tok_embeddings(tokens)
+        self.freqs_cis = self.freqs_cis.to(h.device)
+        freqs_cis = self.freqs_cis[start_pos : start_pos + seqlen]
+
+        for layer in self.layers:
+            h = layer(h, start_pos, freqs_cis, mask)
+        h = self.norm(h)
+
+        output = self.output(h)
+
+        return output
