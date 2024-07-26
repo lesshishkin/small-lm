8d49d7cf73735a6599cae313b27f5e9258a2cfa3
lesshishkin <lesshishkin@gmail.com>
2024-07-26 21:01:01 +0300

    model config

--- configs/model_config.py
+++ configs/model_config.py
@@ -1,17 +1,17 @@
 from easydict import EasyDict
 
 model_cfg = EasyDict()
-model_cfg.name = 'TinyLLM'
+model_cfg.name = 'TinyLLM2'
 
 model_cfg.dim = 512
 model_cfg.d_ff = 2048
 model_cfg.max_seq_len = 2048   # for RoPE
-model_cfg.n_heads = 8
+model_cfg.n_heads = 16
 model_cfg.n_kv_heads = 4       # GMQA
-model_cfg.n_layers = 12
+model_cfg.n_layers = 7
 model_cfg.eps = 1e-6
 model_cfg.dropout_rate = 0.1
 model_cfg.rope_theta = 100_000.0
 model_cfg.multiple_of = 256
 model_cfg.ffn_dim_multiplier = None
-model_cfg.norm_eps = 1e-5
+model_cfg.norm_eps = 1e-6
