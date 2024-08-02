6e3e5e5f620caf98b35d8b1dcbe889f5b9576131
lesshishkin <lesshishkin@gmail.com>
2024-08-02 12:25:27 +0300

    local data path

--- configs/data_config.py
+++ configs/data_config.py
@@ -5,8 +5,8 @@ import os
 # TinyStories Dataset
 data_cfg = EasyDict()
 data_cfg.name = 'TinyStoriesDataset'
-data_cfg.path_to_data = '/kaggle/input/tinystories'
-# data_cfg.path_to_data = 'data/tinystories'
+# data_cfg.path_to_data = '/kaggle/input/tinystories'
+data_cfg.path_to_data = 'data/tinystories'
 data_cfg.tokenized_train_data_path = os.path.join(data_cfg.path_to_data, 'ru_part_2.pickle')
 data_cfg.tokenized_valid_data_path = os.path.join(data_cfg.path_to_data, 'valid_v2_ru_tokenized.pickle')
 data_cfg.vocabulary_size = 25_000
