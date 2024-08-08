e5cb0c1d0bb2919924f4b725c74f8242960c99d9
lesshishkin <lesshishkin@gmail.com>
2024-08-08 12:40:14 +0300

    instruct dataset todo

--- dataset/russian_stories_dataset.py
+++ dataset/russian_stories_dataset.py
@@ -30,3 +30,36 @@ class TinyStoriesDataset(Dataset):
         }
 
         return sample_data
+
+
+class TinyStoriesInstructDataset(Dataset):
+    """A class for Russian Tiny Stories Dataset."""
+    #  todo сделать инстракт датасет. нужно чтобы токены, которые отвечают за часть с инструкцией
+    # в таргете менялись на pad index
+    # то есть нужно знать сколько первых токенов менять
+
+    def __init__(self, config, set_type: SetType):
+        self.config = config
+        self.set_type = set_type
+
+        if self.set_type == SetType.train:
+            self.dataset = read_file(config.tokenized_train_data_path)
+
+        elif self.set_type == SetType.validation:
+            self.dataset = read_file(config.tokenized_valid_data_path)
+
+        else:
+            raise Exception('Unknown set type')
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx: int):
+        sample_data = {
+            'id': idx,
+            'tokens': self.dataset[idx]
+        }
+
+        return sample_data
+
+
